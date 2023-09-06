bd84328bd79b0b6b83049e7b2b44895d0ff18c62
unknown <DanilaHalm>
2023-09-06 17:41:11 +0300

    feat: add nextauth

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ["firebasestorage.googleapis.com"],
     unoptimized: true,
   },
-  output: "export",
+  // output: "export",
 };
 
 module.exports = nextConfig;
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,9 +1,18 @@
 "use client";
 import { ParseContext } from "@/servises/context";
+import { useSession, signOut } from "next-auth/react";
 import DashboardToggle from "./dashboardToggle";
 import Parse from "@/servises/parse";
 
 const Dashboard = () => {
+  const session = useSession();
+  if (session?.data?.user?.email !== process.env.NEXT_PUBLIC_ADMIN_EMAIL && session?.status === "authenticated") {
+    // console.log(session);
+    // signOut({
+    //   callbackUrl: "/",
+    // });
+  }
+
   return (
     <ParseContext.Provider value={Parse}>
       <div className="min-h-screen flex flex-col px-4">
